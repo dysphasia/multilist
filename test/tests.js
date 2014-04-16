7058171bfd5a4c0979afb1f640ed61aa464f12e1
Jason Duncan <jduncan@mmsinc.com>
2014-04-16 09:58:54 -0400

    Filling in test coverage.

--- test/tests.js
+++ test/tests.js
@@ -1,20 +1,54 @@
 (function (T, $) {
+  var $doc, $body, $target;
 
-  var doc = $(document),
-    context = $('#input').multilist();
+  T.testStart(function () {
+    $doc = $(document);
+    $body = $(document.body).append('<div id="target" />');
+    $target = $('div#target');
+  });
 
-  T.test('Test Toggle', function () {
-    expect(2);
-    var firstTab = $('li:first', context),
-        firstTabLink = $('a:first', firstTab),
-        firstTabUl = $('ul:first', firstTab);
+  T.testDone(function () {
+    $target.remove();
+  });
 
-    // First click.
-    firstTabLink.click();
-    T.equals(firstTabUl.css('display'), 'block');
+  /*** INIT ***/
 
-    // Second click.
-    firstTabLink.click();
-    T.equals(firstTabUl.css('display'), 'none');
+  T.test('Test init adds proper attributes to the target', function() {
+    $target.multilist();
+
+    T.equal($target.attr('role'), 'listbox');
+    T.equal($target.attr('aria-multiselectable'), 'true');
+  });
+
+  T.test('Test init shows target', function() {
+    $target.hide();
+
+    $target.multilist();
+
+    T.ok($target.is(':visible'), 'Target should be made visible');
+  });
+
+  /*** OPEN ***/
+
+  T.test('Test clicking on target shows search', function() {
+    $target.multilist();
+
+    $target.click();
+
+    var $search = $('div.search', $target);
+
+    T.ok($search.length == 1, 'Search element should be created');
+    T.ok($search.is(':visible'), 'Search element should be made visible');
+  });
+
+  T.test('Test clicking on target shows items', function() {
+    $target.multilist();
+
+    $target.click();
+
+    var $items = $('div.items', $target);
+
+    T.ok($items.length == 1, 'Items holder element should be created');
+    T.ok($items.is(':visible'), 'Items holder element should be made visible');
   });
 } (QUnit, jQuery));
