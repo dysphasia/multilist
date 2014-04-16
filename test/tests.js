b2e7bcaad4f3ec91a52c80fc5918bd416222a27f
Jason Duncan <jduncan@mmsinc.com>
2014-04-16 13:10:04 -0400

    More test coverage and test organization.  There may be a failing test.

--- test/tests.js
+++ test/tests.js
@@ -5,10 +5,12 @@
     $doc = $(document);
     $body = $(document.body).append('<div id="target" />');
     $target = $('div#target');
+    $.fx.off = true;
   });
 
   T.testDone(function () {
     $target.remove();
+    $.fx.off = false;
   });
 
   /*** INIT ***/
@@ -30,29 +32,64 @@
     T.ok($target.is(':visible'), 'Target should be made visible');
   });
 
-  /*** OPEN ***/
+  /*** CLICK WHEN OPEN ***/
 
-  T.module('open');
+  T.module('click when closed', {
+    setup: function() {
+      $target.multilist();
+    }
+  });
 
-  T.test('shows search', function() {
-    $target.multilist();
+  T.test('shows and focuses search', function() {
+    // focus never happens unless this is called manually
+    $.fn.multilist.call($target, 'open');
 
-    $target.click();
+    var $searchHolder = $('div.search', $target);
+    var $search = $('input[role="search"]', $searchHolder);
 
-    var $search = $('div.search', $target);
-
+    T.ok($searchHolder.length == 1, 'Search element holder should be created');
+    T.ok($searchHolder.is(':visible'), 'Search element holder should be made visible');
     T.ok($search.length == 1, 'Search element should be created');
-    T.ok($search.is(':visible'), 'Search element should be made visible');
+    T.ok($search.is(':focus'), 'Search element should have focus');
   });
 
   T.test('shows items', function() {
-    $target.multilist();
-
     $target.click();
 
     var $items = $('div.items', $target);
 
     T.ok($items.length == 1, 'Items holder element should be created');
     T.ok($items.is(':visible'), 'Items holder element should be made visible');
+  });
+
+  T.test('adds `opened\' css class', function() {
+    // have to call manually for whatever reason
+    $.fn.multilist.call($target, 'open');
+
+    T.ok($target.hasClass('opened'), 'Target should have `opened\' css class');
+  });
+
+  /*** CLICK WHEN CLOSED ***/
+
+  T.module('click when open', {
+    setup: function() {
+      $target.multilist();
+      $.fn.multilist.call($target, 'open');
+    }
+  });
+
+  T.test('clears the search', function() {
+    var $searchHolder = $('div.search', $target);
+    var $search = $('input[role="search"]', $searchHolder);
+    var $items = $('div.items', $target);
+    $search.val('foo');
+    $items.addClass('filtered');
+
+    // have to call manually for whatever reason
+    $.fn.multilist.call($target, 'close');
+
+    T.ok(!$searchHolder.is(':visible'), 'Search element holder should be hidden');
+    T.equal($search.val(), '');
+    T.ok(!$items.hasClass('filtered'), 'Items holder element should no longer have `filtered\' css class');
   });
 } (QUnit, jQuery));
